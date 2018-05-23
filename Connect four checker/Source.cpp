eeb8b59577526f44530d6dfe2a9b9d3c96def107
Mondotrasho <oscar@peoplepictures.com.au>
2018-05-23 15:13:07 +1000

    split print and fill

--- Connect four checker/Source.cpp
+++ Connect four checker/Source.cpp
@@ -7,15 +7,28 @@ int main()
 {
 	int board[7][6]; // the board X and Y
 
-	for(int i= 0; i <=6;i++) //fill it goes through Y
+//## FILL ##
+	for (int i = 0; i <= 6; i++) //fill it goes through Y
 	{
-		for(int j = 0;j <=5; j++)	//goes through X
+		for (int j = 0; j <= 5; j++)	//goes through X
 		{
 			board[j][i] = (rand() % 2); //fills pick 0 or 1 
-			std::cout << board[j][i]; 
+		}
+	}
+
+//## PRINT ##
+
+	for (int i = 0; i <= 5; i++) 
+	{
+		for (int j = 0; j <= 6; j++)
+		{
+			
+			std::cout << board[i][j];
 		}
 		std::cout << std::endl;
 	}
+
+//## CHECK ##
 
 //	for(int i;i<=6;i++) //check it
 	{
