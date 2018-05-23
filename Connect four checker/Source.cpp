9812337d3509543b74897f66a11eca5965df1668
Mondotrasho <oscar@peoplepictures.com.au>
2018-05-23 15:06:59 +1000

    Horrible errors

--- Connect four checker/Source.cpp
+++ Connect four checker/Source.cpp
@@ -5,21 +5,21 @@
 
 int main()
 {
-	int board[6][7]; // the board
+	int board[7][6]; // the board X and Y
 
-	for(int i= 0; i <=6;i++) //fill it
+	for(int i= 0; i <=6;i++) //fill it goes through Y
 	{
-		for(int j = 0;j <=5; j++)
+		for(int j = 0;j <=5; j++)	//goes through X
 		{
-			board[i][j] = (rand() % 2);
-			std::cout << board[j][i];
+			board[j][i] = (rand() % 2); //fills pick 0 or 1 
+			std::cout << board[j][i]; 
 		}
 		std::cout << std::endl;
 	}
 
-	for(int i;i<=6;i++) //check it
+//	for(int i;i<=6;i++) //check it
 	{
-		for(int j;j <= 5;j++)
+//		for(int j;j <= 5;j++)
 		{
 			
 		}
